bf59bdb88efb37a3b59ea36ceda89720db7fd993
Cameron <10779505@my.uvu.edu>
2020-09-06 12:36:10 -0600

    Followed lecture and added moverscript options on cubes, and buttons

--- Cyborg Shrimp/Assets/Scripts/MoverScript.cs
+++ Cyborg Shrimp/Assets/Scripts/MoverScript.cs
@@ -1,22 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class MoverScript : MonoBehaviour
 {
-    public float speed = 3f;
-    public float x, y, z;
-    // Start is called before the first frame update
+    public float speed = 5f;
+    public int score = 100;
+
     void Start()
     {
-        Debug.Log(message: "Hello World!");
+        Debug.Log(message:"Hello World!");
     }
-
+    
     // Update is called once per frame
+
     void Update()
     {
-        x = speed*Input.GetAxis("Horizontal")*Time.deltaTime;
-        y = speed*Input.GetAxis("Vertical")*Time.deltaTime;
-        transform.Translate(x, y, z);
+        var y = speed * Time.deltaTime * Input.GetAxis("Vertical");
+        var x = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        var z = 0;
+        transform.Translate(x,y,z);
+    }
+
+    public void Up()
+    {
+        transform.Translate(x: 0, y:speed , z:0);
+    }
+    public void Down()
+    {
+        transform.Translate(x: 0, y:-speed , z:0);
     }
 }
