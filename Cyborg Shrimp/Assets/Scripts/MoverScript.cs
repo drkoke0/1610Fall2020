74eace831a29a34d9ae343f4364fe9504ccc5b4d
Cameron <10779505@my.uvu.edu>
2020-09-19 15:33:44 -0600

    Lecture Update, moverscript update.

--- Cyborg Shrimp/Assets/Scripts/MoverScript.cs
+++ Cyborg Shrimp/Assets/Scripts/MoverScript.cs
@@ -1,32 +1,22 @@
 ﻿
+using System;
 using UnityEngine;
 
 public class MoverScript : MonoBehaviour
 {
-    public float speed = 5f;
-    public int score = 100;
-
-    void Start()
+    public float moveSpeed;
+    private Vector3 moveDirection;
+    public void Update()
     {
-        Debug.Log(message:"Hello World!");
-    }
-    
-    // Update is called once per frame
-
-    void Update()
-    {
-        var y = speed * Time.deltaTime * Input.GetAxis("Vertical");
-        var x = speed * Time.deltaTime * Input.GetAxis("Horizontal");
-        var z = 0;
-        transform.Translate(x,y,z);
-    }
-
-    public void Up()
-    {
-        transform.Translate(x: 0, y:speed , z:0);
-    }
-    public void Down()
-    {
-        transform.Translate(x: 0, y:-speed , z:0);
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.x = moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            moveDirection.x = -moveSpeed * Time.deltaTime;
+        }
+        transform.Translate(moveDirection);
+        
     }
 }
