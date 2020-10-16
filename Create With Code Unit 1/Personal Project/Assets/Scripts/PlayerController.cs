70cda2bebe7988cab3935302524abf9cc17def19
Cameron <10779505@my.uvu.edu>
2020-10-15 22:33:08 -0600

    Create with code 3 and 4

---
+++ Create With Code Unit 1/Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 3.0f;
+    private float yBound = 6;
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.transform.Translate(Vector3.up * speed * verticalInput);
+        playerRb.transform.Translate(Vector3.right * speed * horizontalInput);
+
+    }
+}
