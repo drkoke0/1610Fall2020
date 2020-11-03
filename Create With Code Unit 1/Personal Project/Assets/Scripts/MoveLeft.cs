a4dbd2bbc6210d78dba29075a531440a7d2d5dd7
Cameron <10779505@my.uvu.edu>
2020-11-02 17:14:08 -0700

    Create with code personal project

---
+++ Create With Code Unit 1/Personal Project/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 2.0f;
+    private Rigidbody objectRb;
+    private float xDestroy = -10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectRb.transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if (transform.position.x < xDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with enemy.")
+                }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
