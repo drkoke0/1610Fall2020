1fd43f61a536a78f9e1a39bc2c00d69e0d6ce12b
Cameron <10779505@my.uvu.edu>
2020-11-17 20:57:20 -0700

    Weapon Switch Homework

---
+++ Cyborg Shrimp/Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public enum Weapons
+    {
+        Pistol,
+        Shotgun,
+        HuntingRifle,
+        AssaultRifle,
+        Riveter,
+        Mortar,
+    }
+
+    public Weapons currentWeapon;
+
+    public void RunCurrentWeapon()
+    {
+        switch (currentWeapon)
+        {
+            case Weapons.Pistol:
+                break;
+            //function
+            case Weapons.Shotgun:
+                break;
+            case Weapons.HuntingRifle:
+                break;
+            case Weapons.AssaultRifle:
+                break;
+            case Weapons.Riveter:
+                break;
+            case Weapons.Mortar:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
