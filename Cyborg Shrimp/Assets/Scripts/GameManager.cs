7c25a213d90652b9096988a9ed3492a8e6479b64
Cameron <10779505@my.uvu.edu>
2020-12-03 20:12:10 -0700

    Final Project Work

--- Cyborg Shrimp/Assets/Scripts/GameManager.cs
+++ Cyborg Shrimp/Assets/Scripts/GameManager.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button startButton;
+    private int score;
+    public bool isGameActive;
+    public GameObject titleScreen;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +25,18 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        startButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
     }
 }
