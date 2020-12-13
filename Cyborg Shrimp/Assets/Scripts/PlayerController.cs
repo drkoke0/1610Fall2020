8da9d0d27abca88ac3750ca3f0d2cccf51ca01eb
Cameron <10779505@my.uvu.edu>
2020-12-12 21:58:37 -0700

    Final Project Work

--- Cyborg Shrimp/Assets/Scripts/PlayerController.cs
+++ Cyborg Shrimp/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int speed;
-
-
-    
+    private float speed = 1.0f;
+    private float yBound = 6;
+    //Keep Player in Bounds
+    public float xRange = 100;
+    private Rigidbody playerRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
         
+        playerRb.transform.Translate(Vector3.forward * speed * horizontalInput);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 100, ForceMode.Impulse);
+        }
     }
 }
